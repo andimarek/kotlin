d33dfd865684f1dc439ec75e3aed822994e3a038
Alexander Udalov <alexander.udalov@jetbrains.com>
2026-04-17 10:20:04 +0200

    Reflection: basic support of companion block members and extensions
    Ensure that companion block members are present in `KClass.members` (but
    not inherited in subclasses), support `parameters` and `call` for
    companion block members and companion extensions.
    
     #KT-84275
     #KT-85902 Fixed
     #KT-85903 Fixed

--- core/reflection.jvm/src/kotlin/reflect/jvm/internal/KotlinKProperty.kt
+++ core/reflection.jvm/src/kotlin/reflect/jvm/internal/KotlinKProperty.kt
@@ -6,6 +6,7 @@
 package kotlin.reflect.jvm.internal
 
 import org.jetbrains.kotlin.descriptors.runtime.structure.safeClassLoader
+import org.jetbrains.kotlin.metadata.deserialization.Flags
 import java.lang.reflect.*
 import kotlin.LazyThreadSafetyMode.PUBLICATION
 import kotlin.metadata.*
@@ -22,16 +23,24 @@ internal abstract class KotlinKProperty<out V>(
 ) : KotlinKCallable<V>(overriddenStorage), ReflectKProperty<V> {
     override val name: String get() = kmProperty.name
 
+    private val extensionReceiverType: KmType? by lazy(PUBLICATION) {
+        kmProperty.receiverParameterType.takeUnless {
+            // Replace with an access to `KmProperty.isStatic` when that API appears.
+            val flags = KmProperty::class.java.getDeclaredField("flags").apply { isAccessible = true }.get(kmProperty) as Int
+            Flags.IS_STATIC_PROPERTY.get(flags)
+        }
+    }
+
     override val allParameters: List<KParameter> by lazy(PUBLICATION) {
         computeParameters(
-            kmProperty.contextParameters, kmProperty.receiverParameterType, valueParameters = emptyList(), typeParameterTable.value,
+            kmProperty.contextParameters, extensionReceiverType, valueParameters = emptyList(), typeParameterTable.value,
             includeReceivers = true,
         )
     }
 
     override val parameters: List<KParameter> by lazy(PUBLICATION) {
         if (isBound) computeParameters(
-            kmProperty.contextParameters, kmProperty.receiverParameterType, valueParameters = emptyList(), typeParameterTable.value,
+            kmProperty.contextParameters, extensionReceiverType, valueParameters = emptyList(), typeParameterTable.value,
             includeReceivers = false,
         )
         else allParameters
