160b1653247b5a2940fd805ef6012a12553619a5
Sergej Jaskiewicz <sergej.jaskiewicz@jetbrains.com>
2026-05-04 20:09:47 +0200

    [JS] Support transitive export in Analisys API-based TypeScript Export
    In the Analysis-API based implementation, we treat
    the `@JsImplicitExport` annotations almost the same as the regular
    `@JsExport`. This is because in IR-based implementation we have
    `ImplicitlyExportedDeclarationsMarkingLowering`, which converts
    all `@JsImplicitExport(couldBeConvertedToExplicitExport = true)`
    annotations to a regular `@JsExport`, if the annotated declarations
    are referenced in types from explicitly exported declarations.
    
    Basically, we now have a two-step process: building the export model
    from explicitly exported declarations while simultaneously collecting
    the referenced `@JsImplicitExport` ones, and then exporting
    the collected declarations.
    
    As for the `@JsImplicitExport(couldBeConvertedToExplicitExport = false)`
    case, we never have those in AA-based export, because such annotations
    are generated by the same
    `ImplicitlyExportedDeclarationsMarkingLowering` and are only used
    when 'strict implicit export' is enabled. And we don't intend
    to support strict implicit export in this new AA-based export,
    because it didn't prove to be useful.
    
    (Having `@JsImplicitExport` serve two quite different purposes was
    a mistake which will be fixed when we drop the IR-based export.)
    
    ^KT-82266 Fixed

--- js/js.translator/testData/typescript-export/js/variance-annotations/variance-annotations.aa.d.ts
+++ js/js.translator/testData/typescript-export/js/variance-annotations/variance-annotations.aa.d.ts
@@ -1,7 +1,6 @@
 declare namespace JS_TESTS {
     type Nullable<T> = T | null | undefined
     function KtSingleton<T>(): T & (abstract new() => any);
-
     namespace foo {
         class Covariant<out T> {
             constructor(value: T);
@@ -47,3 +46,5 @@ declare namespace JS_TESTS {
         }
     }
 }
+
+
