81b438e9b60d0e3302d8c27a0920b8360c9afb70
Nikita Klimenko <nikita.klimenko@jetbrains.com>
2026-05-15 14:22:12 +0300

    [KDF] Support prototype of DataFrame.filterIsInstance
    API is simple and useful, good chance it'll end up in the library at some point.
    It'd take 2-4 month from the decision to implement plugin support to stable release
    Idea here is to potentially speed up this process
    if final API will not change.
    If it changes, we simply replace it with new implementation and wait.

---
+++ plugins/kotlin-dataframe/testData/box/addToLib/filterIsInstance.kt
@@ -0,0 +1,28 @@
+import org.jetbrains.kotlinx.dataframe.*
+import org.jetbrains.kotlinx.dataframe.annotations.*
+import org.jetbrains.kotlinx.dataframe.api.*
+import org.jetbrains.kotlinx.dataframe.io.*
+import kotlin.reflect.typeOf
+
+@Refine
+@Interpretable("FilterIsInstance")
+public inline fun <T, reified R> DataFrame<T>.filterIsInstance(noinline columns: ColumnsSelector<T, *>): DataFrame<*> {
+    val resolved = getColumns(columns)
+    val size = resolved.firstOrNull()?.size() ?: return this
+    val indices = ArrayList<Int>()
+    repeat(size) { row ->
+        if (resolved.all { it[row] is R }) indices.add(row)
+    }
+    return getRows(indices).replace(columns = columns).with { DataColumn.createByType(it.name(), it.values() as List<R>, typeOf<R>()) }
+}
+
+fun box(): String {
+    val df = (0 until 5).toDataFrame {
+        "index" from { it }
+        "mixed" from { (if (it % 2 == 0) it.toDouble() else it) as Any }
+    }
+        .filterIsInstance<_, Int> { mixed }
+    val i: Int = df[0].mixed
+    df.compareSchemas(strict = true)
+    return "OK"
+}
