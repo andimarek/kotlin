bfd2ce35dbd1ef24af29b70e7b3daa5e0cc04440
Alexey Belkov <alexey.belkov@jetbrains.com>
2026-05-18 13:59:03 +0400

    [LL] Add file structure test with dangling modifier in companion block
    Before the fix in the previous commit, this test failed with:
    
    KotlinIllegalArgumentExceptionWithAttachments: No dangling modifier found
    
    KT-86363

---
+++ analysis/low-level-api-fir/testData/fileStructure/companionBlockDanglingModifier.kt
@@ -0,0 +1,9 @@
+/* RootStructureElement */data class Vector(val x: Double, val y: Double)/* DeclarationStructureElement */ {/* ClassDeclarationStructureElement */
+    companion {
+        fun t() {/* DeclarationStructureElement */
+        }
+        for (a in/* DeclarationStructureElement */ 1..10) {/* DeclarationStructureElement */
+
+        }
+    }
+}
