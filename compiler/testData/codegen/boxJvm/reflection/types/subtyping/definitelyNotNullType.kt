9779379b3e831bd956fc5943e11f165da29b12b9
Alexander Udalov <alexander.udalov@jetbrains.com>
2026-05-19 13:25:54 +0200

    fixup! Reflection: fix exception on DNN in subtyping

--- compiler/testData/codegen/boxJvm/reflection/types/subtyping/definitelyNotNullType.kt
+++ compiler/testData/codegen/boxJvm/reflection/types/subtyping/definitelyNotNullType.kt
@@ -1,6 +1,7 @@
 // TARGET_BACKEND: JVM
 // WITH_REFLECT
 
+import kotlin.reflect.KCallable
 import kotlin.reflect.KType
 import kotlin.reflect.full.isSubtypeOf
 import kotlin.reflect.full.isSupertypeOf
@@ -11,11 +12,23 @@ interface A<T> {
     fun f(t: T & Any): T & Any
     fun g(): T & Any
     fun <T> h(): T & Any
+
+    fun t(): T
 }
 
 fun checkSubtype(subtype: KType, supertype: KType) {
     assertTrue(subtype.isSubtypeOf(supertype), "Expected $subtype to be a subtype of $supertype")
     assertTrue(supertype.isSupertypeOf(subtype), "Expected $subtype to be a subtype of $supertype")
+}
+
+fun checkNotSubtype(subtype: KType, supertype: KType) {
+    assertFalse(subtype.isSubtypeOf(supertype), "Expected $subtype NOT to be a subtype of $supertype")
+    assertFalse(supertype.isSupertypeOf(subtype), "Expected $subtype NOT to be a subtype of $supertype")
+}
+
+fun checkEqualTypes(type1: KType, type2: KType) {
+    assertTrue(type1.isSubtypeOf(type2), "Expected $type1 to be a subtype of $type2")
+    assertTrue(type2.isSubtypeOf(type1), "Expected $type2 to be a subtype of $type1")
 }
 
 fun checkUnrelatedTypes(type1: KType, type2: KType) {
@@ -29,16 +42,18 @@ fun box(): String {
     val f = A<*>::f
     val t1 = f.parameters.last().type
     val t2 = f.returnType
-    checkSubtype(t1, t2)
-    checkSubtype(t2, t1)
+    checkEqualTypes(t1, t2)
 
     val g = A<*>::f
     val t3 = g.returnType
-    checkSubtype(t1, t3)
-    checkSubtype(t3, t1)
+    checkEqualTypes(t1, t3)
 
     val h = A::class.members.single { it.name == "h" }.returnType
     checkUnrelatedTypes(t1, h)
+
+    val t = A<*>::t.returnType
+    checkSubtype(t1, t)
+    checkNotSubtype(t, t1)
 
     return "OK"
 }
