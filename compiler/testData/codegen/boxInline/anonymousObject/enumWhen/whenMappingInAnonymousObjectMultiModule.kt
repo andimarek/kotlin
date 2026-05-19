ac63d118a945dbd04d662e5bf526549b6b929629
Andrey Mogilev <andrey.mogilev@jetbrains.com>
2026-05-19 16:21:21 +0200

    fixup! [JVM] add synthetic classes visibility to JVM metadata
    add failed test (D8 error)

---
+++ compiler/testData/codegen/boxInline/anonymousObject/enumWhen/whenMappingInAnonymousObjectMultiModule.kt
@@ -0,0 +1,26 @@
+// MODULE: lib
+// FILE: 1.kt
+
+package a
+
+enum class X {
+    A,
+    B
+}
+
+inline fun test(x: X): String {
+    return object {
+        fun foo(): String =
+            when (x) {
+                X.A -> "OK"
+                else -> "Fail"
+            }
+    }.foo()
+}
+
+// MODULE: app(lib)
+// FILE: 2.kt
+
+import a.*
+
+fun box(): String = test(X.A)
