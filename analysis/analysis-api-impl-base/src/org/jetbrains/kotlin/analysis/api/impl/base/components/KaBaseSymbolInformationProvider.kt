ewok-pulse/saber-e2e-kotlin

[Analysis API] Introduce 'applicableAnnotationTargets'
Unlike the previously available 'annotationApplicableTargets',
the new property doesn't expose the compiler-internal type
'KotlinTarget'.

^KT-84911

analysis/analysis-api-impl-base/src/org/jetbrains/kotlin/analysis/api/impl/base/components/KaBaseSymbolInformationProvider.kt
@@ -5,15 +5,20 @@
 
 package org.jetbrains.kotlin.analysis.api.impl.base.components
 
+import org.jetbrains.kotlin.analysis.api.KaExperimentalApi
 import org.jetbrains.kotlin.analysis.api.KaImplementationDetail
 import org.jetbrains.kotlin.analysis.api.KaSession
 import org.jetbrains.kotlin.analysis.api.components.KaSymbolInformationProvider
 import org.jetbrains.kotlin.analysis.api.lifetime.withValidityAssertion
+import org.jetbrains.kotlin.analysis.api.symbols.KaClassSymbol
 import org.jetbrains.kotlin.analysis.api.symbols.KaKotlinPropertySymbol
 import org.jetbrains.kotlin.analysis.api.symbols.KaSymbol
+import org.jetbrains.kotlin.descriptors.annotations.KotlinTarget
 
 @KaImplementationDetail
 abstract class KaBaseSymbolInformationProvider<T : KaSession> : KaBaseSessionComponent<T>(), KaSymbolInformationProvider {
+    protected abstract fun computeAnnotationApplicableTargets(symbol: KaClassSymbol): Set<KotlinTarget>?
+
     override val KaSymbol.isDeprecated: Boolean
         get() = withValidityAssertion { deprecation != null }
 
@@ -21,4 +26,36 @@ abstract class KaBaseSymbolInformationProvider<T : KaSession> : KaBaseSessionCom
         get() = withValidityAssertion {
             getter?.isInline == true && (isVal || setter?.isInline == true)
         }
+
+    @Deprecated("Use 'applicableAnnotationTargets' instead", level = DeprecationLevel.HIDDEN)
+    override val KaClassSymbol.annotationApplicableTargets: Set<KotlinTarget>?
+        get() = withValidityAssertion {
+            computeAnnotationApplicableTargets(this)
+        }
+
+    @KaExperimentalApi
+    override val KaClassSymbol.applicableAnnotationTargets: Set<AnnotationTarget>?
+        get() = withValidityAssertion {
+            computeAnnotationApplicableTargets(this)?.mapNotNullTo(mutableSetOf()) { it.toAnnotationTarget() }
+        }
+}
+
+@KaImplementationDetail
+fun KotlinTarget.toAnnotationTarget(): AnnotationTarget? = when (this) {
+    KotlinTarget.CLASS -> AnnotationTarget.CLASS
+    KotlinTarget.ANNOTATION_CLASS -> AnnotationTarget.ANNOTATION_CLASS
+    KotlinTarget.TYPE_PARAMETER -> AnnotationTarget.TYPE_PARAMETER
+    KotlinTarget.PROPERTY -> AnnotationTarget.PROPERTY
+    KotlinTarget.FIELD -> AnnotationTarget.FIELD
+    KotlinTarget.LOCAL_VARIABLE -> AnnotationTarget.LOCAL_VARIABLE
+    KotlinTarget.VALUE_PARAMETER -> AnnotationTarget.VALUE_PARAMETER
+    KotlinTarget.CONSTRUCTOR -> AnnotationTarget.CONSTRUCTOR
+    KotlinTarget.FUNCTION -> AnnotationTarget.FUNCTION
+    KotlinTarget.PROPERTY_GETTER -> AnnotationTarget.PROPERTY_GETTER
+    KotlinTarget.PROPERTY_SETTER -> AnnotationTarget.PROPERTY_SETTER
+    KotlinTarget.TYPE -> AnnotationTarget.TYPE
+    KotlinTarget.EXPRESSION -> AnnotationTarget.EXPRESSION
+    KotlinTarget.FILE -> AnnotationTarget.FILE
+    KotlinTarget.TYPEALIAS -> AnnotationTarget.TYPEALIAS
+    else -> null
 }

analysis/analysis-api/src/org/jetbrains/kotlin/analysis/api/components/KaSymbolInformationProvider.kt
@@ -115,7 +115,15 @@ public interface KaSymbolInformationProvider : KaSessionComponent {
      * A set of applicable targets for an annotation class symbol, or `null` if the symbol is not an annotation class.
      */
     @KaExperimentalApi
+    @Deprecated("Use 'applicableAnnotationTargets' instead", level = DeprecationLevel.HIDDEN)
+    @KaNoContextParameterBridgeRequired
     public val KaClassSymbol.annotationApplicableTargets: Set<KotlinTarget>?
+
+    /**
+     * A set of applicable targets for an annotation class symbol, or `null` if the symbol is not an annotation class.
+     */
+    @KaExperimentalApi
+    public val KaClassSymbol.applicableAnnotationTargets: Set<AnnotationTarget>?
 
     /**
      * Whether the property is an [inline property](https://kotlinlang.org/docs/inline-functions.html#inline-properties).
@@ -222,6 +230,22 @@ public sealed class KaReturnValueStatus(public val name: String) {
 }
 
 /**
+ * A set of applicable targets for an annotation class symbol, or `null` if the symbol is not an annotation class.
+ */
+@Deprecated("Use 'applicableAnnotationTargets' instead", level = DeprecationLevel.HIDDEN)
+@KaExperimentalApi
+@KaContextParameterApi
+@KaCustomContextParameterBridge
+context(session: KaSession)
+public val KaClassSymbol.annotationApplicableTargets: Set<KotlinTarget>?
+    get() {
+        @Suppress("UNCHECKED_CAST")
+        @OptIn(KaSessionComponentImplementationDetail::class)
+        return KaSymbolInformationProvider::class.java.getDeclaredMethod("getAnnotationApplicableTargets", KaClassSymbol::class.java)
+            .invoke(session, this) as Set<KotlinTarget>?
+    }
+
+/**
  * The deprecation status of the given symbol, or `null` if the symbol is not deprecated.
  *
  * This considers deprecation annotations applied to the symbol itself and, for property-related
@@ -302,8 +326,8 @@ public fun KaSymbol.deprecation(useSiteTarget: KaAnnotationUseSiteTarget?): KaDe
 @KaExperimentalApi
 @KaContextParameterApi
 context(session: KaSession)
-public val KaClassSymbol.annotationApplicableTargets: Set<KotlinTarget>?
-    get() = with(session) { annotationApplicableTargets }
+public val KaClassSymbol.applicableAnnotationTargets: Set<AnnotationTarget>?
+    get() = with(session) { applicableAnnotationTargets }
 
 /**
  * Whether the property is an [inline property](https://kotlinlang.org/docs/inline-functions.html#inline-properties).

andimarek/kotlin

analysis/analysis-api-impl-base/src/org/jetbrains/kotlin/analysis/api/impl/base/components/KaBaseSymbolInformationProvider.kt
@@ -5,15 +5,21 @@
 
 package org.jetbrains.kotlin.analysis.api.impl.base.components
 
+import org.jetbrains.kotlin.analysis.api.KaExperimentalApi
 import org.jetbrains.kotlin.analysis.api.KaImplementationDetail
 import org.jetbrains.kotlin.analysis.api.KaSession
+import org.jetbrains.kotlin.analysis.api.annotations.KaAnnotationTarget
 import org.jetbrains.kotlin.analysis.api.components.KaSymbolInformationProvider
 import org.jetbrains.kotlin.analysis.api.lifetime.withValidityAssertion
+import org.jetbrains.kotlin.analysis.api.symbols.KaClassSymbol
 import org.jetbrains.kotlin.analysis.api.symbols.KaKotlinPropertySymbol
 import org.jetbrains.kotlin.analysis.api.symbols.KaSymbol
+import org.jetbrains.kotlin.descriptors.annotations.KotlinTarget
 
 @KaImplementationDetail
 abstract class KaBaseSymbolInformationProvider<T : KaSession> : KaBaseSessionComponent<T>(), KaSymbolInformationProvider {
+    protected abstract fun computeAnnotationApplicableTargets(symbol: KaClassSymbol): Set<KotlinTarget>?
+
     override val KaSymbol.isDeprecated: Boolean
         get() = withValidityAssertion { deprecation != null }
 
@@ -21,4 +27,36 @@ abstract class KaBaseSymbolInformationProvider<T : KaSession> : KaBaseSessionCom
         get() = withValidityAssertion {
             getter?.isInline == true && (isVal || setter?.isInline == true)
         }
+
+    @Deprecated("Use 'applicableAnnotationTargets' instead", level = DeprecationLevel.HIDDEN)
+    override val KaClassSymbol.annotationApplicableTargets: Set<KotlinTarget>?
+        get() = withValidityAssertion {
+            computeAnnotationApplicableTargets(this)
+        }
+
+    @KaExperimentalApi
+    override val KaClassSymbol.applicableAnnotationTargets: Set<KaAnnotationTarget>?
+        get() = withValidityAssertion {
+            computeAnnotationApplicableTargets(this)?.mapNotNullTo(mutableSetOf()) { it.toKaAnnotationTarget() }
+        }
+}
+
+@KaImplementationDetail
+fun KotlinTarget.toKaAnnotationTarget(): KaAnnotationTarget? = when (this) {
+    KotlinTarget.CLASS -> KaAnnotationTarget.CLASS
+    KotlinTarget.ANNOTATION_CLASS -> KaAnnotationTarget.ANNOTATION_CLASS
+    KotlinTarget.TYPE_PARAMETER -> KaAnnotationTarget.TYPE_PARAMETER
+    KotlinTarget.PROPERTY -> KaAnnotationTarget.PROPERTY
+    KotlinTarget.FIELD -> KaAnnotationTarget.FIELD
+    KotlinTarget.LOCAL_VARIABLE -> KaAnnotationTarget.LOCAL_VARIABLE
+    KotlinTarget.VALUE_PARAMETER -> KaAnnotationTarget.VALUE_PARAMETER
+    KotlinTarget.CONSTRUCTOR -> KaAnnotationTarget.CONSTRUCTOR
+    KotlinTarget.FUNCTION -> KaAnnotationTarget.FUNCTION
+    KotlinTarget.PROPERTY_GETTER -> KaAnnotationTarget.PROPERTY_GETTER
+    KotlinTarget.PROPERTY_SETTER -> KaAnnotationTarget.PROPERTY_SETTER
+    KotlinTarget.TYPE -> KaAnnotationTarget.TYPE
+    KotlinTarget.EXPRESSION -> KaAnnotationTarget.EXPRESSION
+    KotlinTarget.FILE -> KaAnnotationTarget.FILE
+    KotlinTarget.TYPEALIAS -> KaAnnotationTarget.TYPEALIAS
+    else -> null
 }

analysis/analysis-api/src/org/jetbrains/kotlin/analysis/api/components/KaSymbolInformationProvider.kt
@@ -6,6 +6,7 @@
 package org.jetbrains.kotlin.analysis.api.components
 
 import org.jetbrains.kotlin.analysis.api.*
+import org.jetbrains.kotlin.analysis.api.annotations.KaAnnotationTarget
 import org.jetbrains.kotlin.analysis.api.symbols.*
 import org.jetbrains.kotlin.descriptors.annotations.AnnotationUseSiteTarget
 import org.jetbrains.kotlin.descriptors.annotations.KotlinTarget
@@ -104,7 +105,15 @@ public interface KaSymbolInformationProvider : KaSessionComponent {
      * A set of applicable targets for an annotation class symbol, or `null` if the symbol is not an annotation class.
      */
     @KaExperimentalApi
+    @Deprecated("Use 'applicableAnnotationTargets' instead", level = DeprecationLevel.HIDDEN)
+    @KaNoContextParameterBridgeRequired
     public val KaClassSymbol.annotationApplicableTargets: Set<KotlinTarget>?
+
+    /**
+     * A set of applicable targets for an annotation class symbol, or `null` if the symbol is not an annotation class.
+     */
+    @KaExperimentalApi
+    public val KaClassSymbol.applicableAnnotationTargets: Set<KaAnnotationTarget>?
 
     /**
      * Whether the property is an [inline property](https://kotlinlang.org/docs/inline-functions.html#inline-properties).
@@ -219,6 +228,22 @@ public sealed class KaReturnValueStatus(public val name: String) {
 }
 
 /**
+ * A set of applicable targets for an annotation class symbol, or `null` if the symbol is not an annotation class.
+ */
+@Deprecated("Use 'applicableAnnotationTargets' instead", level = DeprecationLevel.HIDDEN)
+@KaExperimentalApi
+@KaContextParameterApi
+@KaCustomContextParameterBridge
+context(session: KaSession)
+public val KaClassSymbol.annotationApplicableTargets: Set<KotlinTarget>?
+    get() {
+        @Suppress("UNCHECKED_CAST")
+        @OptIn(KaSessionComponentImplementationDetail::class)
+        return KaSymbolInformationProvider::class.java.getDeclaredMethod("getAnnotationApplicableTargets", KaClassSymbol::class.java)
+            .invoke(session, this) as Set<KotlinTarget>?
+    }
+
+/**
  * The deprecation status of the given symbol, or `null` if the symbol is not deprecated.
  *
  * This considers deprecation annotations applied to the symbol itself and, for property-related
@@ -280,8 +305,8 @@ public val KaNamedFunctionSymbol.canBeOperator: Boolean
 @KaExperimentalApi
 @KaContextParameterApi
 context(session: KaSession)
-public val KaClassSymbol.annotationApplicableTargets: Set<KotlinTarget>?
-    get() = with(session) { annotationApplicableTargets }
+public val KaClassSymbol.applicableAnnotationTargets: Set<KaAnnotationTarget>?
+    get() = with(session) { applicableAnnotationTargets }
 
 /**
  * Whether the property is an [inline property](https://kotlinlang.org/docs/inline-functions.html#inline-properties).