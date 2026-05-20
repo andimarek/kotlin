272e3664f2af447bdcee4ab791043dd7d366a65e
cristiangarcia <Cristian.Garcia-Marin@jetbrains.com>
2026-05-20 23:23:40 +0200

    [Build] Make :tools:binary-compatibility-validator:test cacheable

--- libraries/tools/binary-compatibility-validator/build.gradle.kts
+++ libraries/tools/binary-compatibility-validator/build.gradle.kts
@@ -1,8 +1,10 @@
 plugins {
     kotlin("jvm")
+    id("test-inputs-check")
 }
 
 val testArtifacts by configurations.creating
+val nativeStdlib by configurations.creating
 
 dependencies {
     api(libs.kotlinx.bcv)
@@ -15,11 +17,18 @@ dependencies {
     }
 
     testImplementation(kotlinTest("junit"))
+    testImplementation(testFixtures(project(":compiler:test-infrastructure-utils")))
 
     testArtifacts(project(":kotlin-stdlib"))
     testArtifacts(project(":kotlin-stdlib-jdk7"))
     testArtifacts(project(":kotlin-stdlib-jdk8"))
     testArtifacts(project(":kotlin-reflect"))
+    if (kotlinBuildProperties.isKotlinNativeEnabled.get()) {
+        testArtifacts(project(":kotlin-stdlib", "jsRuntimeElements"))
+        testArtifacts(project(":kotlin-stdlib", "wasmJsRuntimeElements"))
+        testArtifacts(project(":kotlin-stdlib", "wasmWasiRuntimeElements"))
+        nativeStdlib(project(":kotlin-native:runtime", "nativeStdlib"))
+    }
 }
 
 sourceSets {
@@ -30,16 +39,25 @@ sourceSets {
     }
 }
 
-val test by tasks.existing(Test::class) {
-    dependsOn(testArtifacts)
-    dependsOn(":kotlin-stdlib:assemble")
-    if (kotlinBuildProperties.isKotlinNativeEnabled.get()) {
-        dependsOn(":kotlin-native:runtime:nativeStdlib")
-    }
-
+tasks.test {
     systemProperty("native.enabled", kotlinBuildProperties.isKotlinNativeEnabled.get())
     systemProperty("overwrite.output", project.providers.gradleProperty("overwrite.output").orNull ?: System.getProperty("overwrite.output", "false"))
     systemProperty("kotlinVersion", project.version)
-    systemProperty("testCasesClassesDirs", sourceSets["test"].output.classesDirs.asPath)
     jvmArgs("-ea")
+    addClasspathProperty("testArtifacts") {
+        from(testArtifacts)
+    }
+    addDirectoryProperty("reference-public-api") {
+        set(project.layout.projectDirectory.dir("reference-public-api"))
+    }
+    addDirectoryProperty("klib-public-api") {
+        set(project.layout.projectDirectory.dir("klib-public-api"))
+    }
+    addClasspathProperty("testCasesClassesDirs") {
+        from(sourceSets["test"].output.classesDirs)
+    }
+    inputs.dir(layout.projectDirectory.dir("src/test/kotlin/cases"))
+        .withPathSensitivity(PathSensitivity.RELATIVE)
+        .withPropertyName("testCasesDir")
+    addClasspathProperty(nativeStdlib, "nativeStdlib")
 }
